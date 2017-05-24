2b80269866f291bbd4f9eb422e12b526793dc8ab
arunkpskpm <arunkpskpm@gmail.com>
2017-05-24 17:52:47 +0800

    Add files via upload

---
+++ solaris_mem_usage_notifier.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+serverName=`uname -n`
+THRESHOLD=80
+MAILER="/usr/bin/mailx"
+MAIL="arun.kp@hpe.com"
+DATE=`date`
+# totalRAM variable must be defined in MB(Megabyte). Use command prtdiag |grep "Memory size" |awk '{print $3}'` to get value in MB
+totalRAM=65536 #in MB
+# calculating Memory usage
+freeRAMpages=`sar -r 1 10|tail -1|awk '{print $2 }'`             # in pages
+freeRAM=`expr $freeRAMpages \* 8192 / 1024 / 1024 \* 100`    # enable this line if the server running on SPARC hardware
+# freeRAM=`expr $freeRAMpages \* 4096 / 1024 / 1024 \* 100`  # enable this line if the server running on x86 platform
+freeRAM_PERCENT=`expr $freeRAM / $totalRAM`
+usedRAM_PERCENT=`expr 100 - $freeRAM_PERCENT`
+messageBody="Hi Team,
+
+This is to notify that memory usage on server $serverName has crossed the configured threshold. Please take necessary action to control the usage
+
+Thanks,
+UNIX Team "
+
+if [ $usedRAM_PERCENT -ge $THRESHOLD ]
+then
+echo "$messageBody"|$MAILER -s "Total Memory usage exceeds $THRESHOLD% threshold for server $serverName. Current usage: $usedRAM_PERCENT%" $MAIL
+fi
